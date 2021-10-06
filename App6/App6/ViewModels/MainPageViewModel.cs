14cf4fc47c0b5d6ed1b3e186688da442330cf96c
farchr186 <2018371104@uteq.edu.mx>
2021-10-06 00:15:36 -0500

    .

---
+++ App6/App6/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,66 @@
+﻿using App6.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App6.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        Command _loginCommand;
+        private bool errorLogin;
+
+        public MainPageViewModel(INavigation navigation) : base(navigation)
+        {
+
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged();
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged();              
+            }
+        }
+
+        public Command LoginCommand
+        {
+            get => _loginCommand ?? (_loginCommand = new Command(GoTologinAction));
+        }
+        public bool Error
+        {
+            get => errorLogin;
+            set
+            {
+                errorLogin = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void GoTologinAction()
+        {
+            Error = !(string.Equals(Email, "app6@gmail.com") && string.Equals(Password, "inicio1234"));
+
+            if (!Error)
+            {
+                Email = string.Empty;
+                Password = string.Empty;
+                Navigation.PushAsync(new PaginaInicio());
+            }
+        }
+    }
+}
---
+++ App6/App6/ViewModels/PaginaInicioViewModel.cs
@@ -0,0 +1,34 @@
+﻿using App6.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App6.ViewModels
+{
+    public class PaginaInicioViewModel : BaseViewModel
+    {
+        Command _goToRoot;
+        Command _goToPerfil;
+
+        public PaginaInicioViewModel(INavigation navigation) : base(navigation)
+        {
+            Navigation = navigation ?? App.Navigation;
+        }
+
+        public Command GoToPerfilCommand { get => _goToPerfil ?? (_goToPerfil = new Command(GoToPerfilAction)); }
+
+        private void GoToPerfilAction()
+        {
+            Navigation.PushAsync(new NavigationPage(new PaginaPerfil()));
+        }
+
+        public Command GoToRootCommand { get => _goToRoot ?? (_goToRoot = new Command(GoToRootAction)); }
+
+        private void GoToRootAction()
+        {
+            Navigation.PopToRootAsync();
+        }
+
+    }
+}
